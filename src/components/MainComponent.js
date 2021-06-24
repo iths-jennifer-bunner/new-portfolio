78595c758aee67582a8442dd0877ecb6836632d4
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-06-24 17:20:16 +0200

    fixed banner

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/MainComponent.scss";
+
+function MainComponent() {
+  return (
+    <>
+      <div className="main__titleBox">
+        <h1 className="main__title main__title--firstWord">SELECTED</h1>
+        <h1 className="main__title main__title--stroke"> PROJECTS : </h1>
+      </div>
+    </>
+  );
+}
+
+export default MainComponent;
